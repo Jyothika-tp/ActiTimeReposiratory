efe54d4186bb39cac53d06bfa1e1104f68f55267
Jyothi-PC <Jyothi-PC@Vittal-PC>
2017-02-25 18:30:38 +0530

    hello

---
+++ src/acti_maven/B.java
@@ -0,0 +1,30 @@
+package acti_maven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class B
+{
+		@Test
+public void testActiLoginLogout()
+	{
+			System.setProperty("webdriver.gecko.driver", "./driver/geckodriver.exe");
+			WebDriver driver=new FirefoxDriver();
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			driver.get("http://localhost/login.do");
+			driver.findElement(By.id("username")).sendKeys("admin");
+			driver.findElement(By.name("pwd")).sendKeys("manager");
+			driver.findElement(By.id("loginButton")).click();
+			driver.findElement(By.id("logoutLink")).click();
+			driver.quit();
+			
+			
+	}
+
+}
+
+
